c818083b10d0159678c83eaa3c6514a54bb79f68
Nguyen Duc <autoclickvn@gmail.com>
2022-04-03 20:25:20 +0700

    Update app name

--- ios/MapleProTests/MapleProTests.m
+++ ios/MapleProTests/MapleProTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MappleProTests : XCTestCase
+@interface MapleProTests : XCTestCase
 
 @end
 
-@implementation MappleProTests
+@implementation MapleProTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
